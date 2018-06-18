191b38aefdff1b031fbca04d165a2f4325c8052c
karthik137 <raokarthik83@gmail.com>
2018-06-18 22:21:42 +0530

    arrays strings

---
+++ arrays1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [3]string
+	arr[0] = "hello";
+	arr[1] = "World";
+	arr[2] = "Hey";
+
+	fmt.Println(arr[0]);
+
+
+	var a [2]string;
+	a[0] = "My";
+	a[1] = "World";
+
+	fmt.Println(a);
+}
