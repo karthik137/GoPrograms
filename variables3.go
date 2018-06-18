3222b812b8550d4d23afe1e52101c04d27df9e2a
Mphasis <raokarthik83@gmail.com>
2018-06-18 14:46:50 +0530

    print variables value

---
+++ variables3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+
+func main(){
+
+	var a string = "hello";
+	var i,z int = 2,3;
+	var h = true;
+
+
+	fmt.Println("Value of a is : ",a);
+	fmt.Println("Value of i, z is : ",i,z);
+	fmt.Println("Value of h is : ",h);
+
+
+}
