b13142b19d15fa099b5dcb33f29233d9a44ad0d9
Mphasis <raokarthik83@gmail.com>
2018-06-19 11:45:49 +0530

    map manipulation

--- manipulateMap.go
+++ manipulateMap.go
@@ -7,9 +7,11 @@ var map2 = make(map[string]int);
 func main(){
 
 	var map1 = make(map[string]int);
-	//map2 := map[string]int;
-	//var map2 map[string]int;
 	map1["one"] = 1;
-	map2["two"] = 2; 
+	map2["two"] = 2;
 	fmt.Println(map1,map2);
+
+	value, ok := map1["one"]
+
+	fmt.Println(value, ok);
 }
