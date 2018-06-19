842a31178b2d7514d7a194fb024dead64fc2c278
Mphasis <raokarthik83@gmail.com>
2018-06-19 10:32:31 +0530

    function in go

--- functions.go
+++ functions.go
@@ -7,10 +7,28 @@ func firstFunc(a int){
 	fmt.Println("Value of a in firstFunc : ",a);
 }
 
+
+func secondFunc(a int) int {
+	a = 10;
+	return a;
+}
+
+
+func thirdFunc() (int,int){
+	return 11,12;
+}
+
 func main(){
 	//short hand decl of var a
 	a := 10;
 	firstFunc(a);
 	fmt.Println("Value of a in main : ",a);
 	firstFunc(a);
+
+	b := secondFunc(20);
+	fmt.Println("value of b is : ",b);
+
+	c,d := thirdFunc();
+
+	fmt.Println("value of c is ",c,"value of d is ",d);
 }
